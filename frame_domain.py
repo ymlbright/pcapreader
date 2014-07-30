22bbd97cbf4140e3970d5b7fd9dc7b1a74d5924e
bright <bright@bright.(none)>
2014-07-30 15:06:30 +0800

    doamin

--- frame_domain.py
+++ frame_domain.py
@@ -1,4 +1,18 @@
 
 class domain_frame(object):
 	def __init__(self,data):
-		pass
+		self.ID = (ord(data[0])<<8) + ord(data[1])
+		self.Flag = data[2:4]
+		self.QuestionNum = (ord(data[4])<<8) + ord(data[5])
+		self.AnswerNum = (ord(data[6])<<8) + ord(data[7])
+		self.AuthorityNum = (ord(data[8])<<8) + ord(data[9])
+		self.AdditionalNum = (ord(data[10])<<8) + ord(data[11])
+		tlen = data.find('\x00',12)
+		self.Queries = data[12:tlen]
+
+	def read_head(self,data):
+		answer = {}
+		tlen = data.find('\x00')
+		answer['Name'] = data[0:tlen]
+		
+
